24076c6165a0af8fe7bf8ac2bc040d270d1d19c8
suichanwa <augustinnegura746@gmail.com>
2026-06-07 17:46:46 +0300

    rolled back JEI caues EMI ain't supported for latest 26.2.1

---
+++ src/main/java/com/suiseika/solenoid/jei/CrushingRecipeCategory.java
@@ -0,0 +1,77 @@
+package com.suiseika.solenoid.jei;
+
+import com.suiseika.solenoid.energy.EmfBlocks;
+import com.suiseika.solenoid.recipe.CrushingRecipe;
+
+import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.drawable.IDrawableStatic;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.recipe.IFocusGroup;
+import mezz.jei.api.recipe.RecipeType;
+import mezz.jei.api.recipe.category.IRecipeCategory;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.GuiGraphicsExtractor;
+import net.minecraft.network.chat.Component;
+
+public class CrushingRecipeCategory implements IRecipeCategory<CrushingRecipe> {
+    private static final int WIDTH = 120;
+    private static final int HEIGHT = 54;
+    private static final int TEXT_COLOR = 0xFF404040;
+
+    private final IDrawable icon;
+    private final IDrawableStatic arrow;
+
+    public CrushingRecipeCategory(IGuiHelper guiHelper) {
+        this.icon = guiHelper.createDrawableItemLike(EmfBlocks.CRUSHER_ITEM.get());
+        this.arrow = guiHelper.getRecipeArrow();
+    }
+
+    @Override
+    public RecipeType<CrushingRecipe> getRecipeType() {
+        return SolenoidJeiPlugin.CRUSHING;
+    }
+
+    @Override
+    public Component getTitle() {
+        return Component.translatable("gui.solenoid.category.crushing");
+    }
+
+    @Override
+    public int getWidth() {
+        return WIDTH;
+    }
+
+    @Override
+    public int getHeight() {
+        return HEIGHT;
+    }
+
+    @Override
+    public IDrawable getIcon() {
+        return icon;
+    }
+
+    @Override
+    public void setRecipe(IRecipeLayoutBuilder builder, CrushingRecipe recipe, IFocusGroup focuses) {
+        builder.addInputSlot(4, 10)
+                .setStandardSlotBackground()
+                .add(recipe.ingredient());
+
+        builder.addOutputSlot(66, 9)
+                .setOutputSlotBackground()
+                .add(recipe.result().create());
+    }
+
+    @Override
+    public void draw(CrushingRecipe recipe, IRecipeSlotsView recipeSlotsView, GuiGraphicsExtractor guiGraphics, double mouseX, double mouseY) {
+        this.arrow.draw(guiGraphics, 30, 11);
+
+        Font font = Minecraft.getInstance().font;
+        guiGraphics.text(font, Component.translatable("gui.solenoid.recipe.energy", recipe.energy()), 4, 34, TEXT_COLOR, false);
+        guiGraphics.text(font, Component.translatable("gui.solenoid.recipe.time", recipe.time()), 4, 44, TEXT_COLOR, false);
+    }
+}
---
+++ src/main/java/com/suiseika/solenoid/jei/SolenoidJeiPlugin.java
@@ -0,0 +1,73 @@
+package com.suiseika.solenoid.jei;
+
+import com.suiseika.solenoid.Solenoid;
+import com.suiseika.solenoid.energy.EmfBlocks;
+import com.suiseika.solenoid.recipe.CrushingRecipe;
+import com.suiseika.solenoid.recipe.SeparatingRecipe;
+import com.suiseika.solenoid.recipe.SolenoidRecipes;
+
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.constants.RecipeTypes;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.recipe.RecipeType;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
+import mezz.jei.api.registration.IRecipeCategoryRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
+import mezz.jei.common.Internal;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.crafting.RecipeHolder;
+import net.minecraft.world.item.crafting.RecipeMap;
+
+import java.util.List;
+
+@JeiPlugin
+public class SolenoidJeiPlugin implements IModPlugin {
+
+    public static final RecipeType<CrushingRecipe> CRUSHING =
+            new RecipeType<>(ResourceLocation.fromNamespaceAndPath(Solenoid.MODID, "crushing"), CrushingRecipe.class);
+    public static final RecipeType<SeparatingRecipe> SEPARATING =
+            new RecipeType<>(ResourceLocation.fromNamespaceAndPath(Solenoid.MODID, "separating"), SeparatingRecipe.class);
+
+    @Override
+    public ResourceLocation getPluginUid() {
+        return ResourceLocation.fromNamespaceAndPath(Solenoid.MODID, "jei");
+    }
+
+    @Override
+    public void registerCategories(IRecipeCategoryRegistration registration) {
+        IGuiHelper guiHelper = registration.getJeiHelpers().getGuiHelper();
+        registration.addRecipeCategories(
+                new CrushingRecipeCategory(guiHelper),
+                new SeparatingRecipeCategory(guiHelper)
+        );
+    }
+
+    @Override
+    public void registerRecipes(IRecipeRegistration registration) {
+        RecipeMap recipes = Internal.getClientSyncedRecipes();
+        if (recipes == null) {
+            return;
+        }
+
+        List<CrushingRecipe> crushing = recipes.byType(SolenoidRecipes.CRUSHING_TYPE.get())
+                .stream()
+                .map(RecipeHolder::value)
+                .toList();
+        registration.addRecipes(CRUSHING, crushing);
+
+        List<SeparatingRecipe> separating = recipes.byType(SolenoidRecipes.SEPARATING_TYPE.get())
+                .stream()
+                .map(RecipeHolder::value)
+                .toList();
+        registration.addRecipes(SEPARATING, separating);
+    }
+
+    @Override
+    public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
+        registration.addCraftingStation(CRUSHING, EmfBlocks.CRUSHER_ITEM.get());
+        registration.addCraftingStation(SEPARATING, EmfBlocks.SEPARATOR_ITEM.get());
+        registration.addCraftingStation(RecipeTypes.SMELTING, EmfBlocks.INDUCTION_FURNACE_ITEM.get());
+    }
+}
